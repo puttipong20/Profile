5e325c1a7ae46be5480ee285e21cc04fd937921f
puttipong <panomprai11@gmail.com>
2022-12-15 23:25:46 +0700

    first commit

---
+++ src/pages/MyFrom.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <q-page class="column items-center justify-center">
+    <q-card class="create-account-card">
+      <q-form>
+        <q-card-section>
+          <div class="text-h6">Create New Account</div>
+          <div class="text-subtitle1">
+            Fill out the following form to create your new account.
+          </div>
+        </q-card-section>
+        <q-separator inset />
+        <q-card-section class="column q-gutter-md">
+          <q-input label="Name *"></q-input>
+          <q-input label="Email *"></q-input>
+          <q-input
+            label="Phone"
+            mask="(###) ### - ####"
+            fill-mask
+            hint="(###) ### - ####"
+          ></q-input>
+          <q-input label="Password *" type="password"></q-input>
+          <div class="password-criteria q-pa-sm">
+            <div class="text-subtitle2 q-mb-sm">Password Criteria:</div>
+            <div>
+              <q-icon name="cancel" color="negative"></q-icon>
+              Must be at least 12 characters long.
+            </div>
+            <div>
+              <q-icon name="cancel" color="negative"></q-icon>
+              Must contain at least one capital letter.
+            </div>
+            <div>
+              <q-icon name="cancel" color="negative"></q-icon>
+              Must contain at least one number.
+            </div>
+            <div>
+              <q-icon name="cancel" color="negative"></q-icon>
+              Must contain at least one symbol.
+            </div>
+          </div>
+          <q-input label="Confirm Password *" type="password"></q-input>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat>Cancel</q-btn>
+          <q-btn color="primary" type="submit">Create Account</q-btn>
+        </q-card-actions>
+      </q-form>
+    </q-card>
+  </q-page>
+</template>
+
+<style lang="scss">
+.create-account-card {
+  width: 512px;
+}
+.password-criteria {
+  background-color: #efefef;
+  border-radius: 0.5rem;
+}
+</style>
