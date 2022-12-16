4d90750c999b9f69e78927207a27521c80063721
puttipong <panomprai11@gmail.com>
2022-12-16 15:08:09 +0700

    finish

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
   // This option interrupts the configuration hierarchy at this file
   // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
-  root: true,
+  root: false,
 
   parserOptions: {
     parser: '@babel/eslint-parser',
--- src/pages/MyFrom.vue
+++ src/pages/MyFrom.vue
@@ -1,8 +1,7 @@
-
 <template>
   <q-page class="column items-center justify-center">
     <q-card class="create-account-card">
-      <q-form>
+      <q-form autofocus @submit="submitForm">
         <q-card-section>
           <div class="text-h6">Create New Account</div>
           <div class="text-subtitle1">
@@ -11,31 +10,47 @@
         </q-card-section>
         <q-separator inset />
         <q-card-section class="column q-gutter-md">
-          <q-input label="Name *" v-model="FormState.name"></q-input>
-          <q-input label="Email *" v-model="FormState.email"></q-input>
-          <q-input label="Phone" v-model="FormState.phone" mask="(###) ### - ####" fill-mask
-            hint="(###) ### - ####"></q-input>
-          <q-input label="Password *" type="password" v-model="FormState.password.value"></q-input>
+          <q-input label="Name *" v-model="FormState.name" :rules="[
+            (val) => (val && val.length > 0) || 'Name must be filled in.',
+          ]"></q-input>
+          <q-input label="Email *" v-model="FormState.email"
+            :rules="[(val) => validateEmail(val) || 'Must be a valid email.']"></q-input>
+          <q-input label="Phone" v-model="FormState.phone" mask="(###) ### - ####" hint="(###) ### - ####"></q-input>
+          <q-input label="Password *" v-model="FormState.password.value" type="password" :rules="[
+            (val) =>
+              validatePassword(val) || 'Password must meet all criteria.',
+          ]">
+          </q-input>
           <div class="password-criteria q-pa-sm">
             <div class="text-subtitle2 q-mb-sm">Password Criteria:</div>
             <div>
-              <q-icon name="cancel" color="negative"></q-icon>
+              <q-icon :name="ValidPassword.length ? 'check_circle' : 'cancel'"
+                :color="ValidPassword.length ? 'positive' : 'negative'"></q-icon>
               Must be at least 12 characters long.
             </div>
             <div>
-              <q-icon name="cancel" color="negative"></q-icon>
+              <q-icon :name="ValidPassword.capital ? 'check_circle' : 'cancel'"
+                :color="ValidPassword.capital ? 'positive' : 'negative'"></q-icon>
               Must contain at least one capital letter.
             </div>
             <div>
-              <q-icon name="cancel" color="negative"></q-icon>
+              <q-icon :name="ValidPassword.number ? 'check_circle' : 'cancel'"
+                :color="ValidPassword.number ? 'positive' : 'negative'"></q-icon>
               Must contain at least one number.
             </div>
             <div>
-              <q-icon name="cancel" color="negative"></q-icon>
-              Must contain at least one symbol.
+              <q-icon :name="ValidPassword.symbol ? 'check_circle' : 'cancel'"
+                :color="ValidPassword.symbol ? 'positive' : 'negative'"></q-icon>
+              Must contain at least one special character: !@#$%^&*()-_+=
             </div>
           </div>
-          <q-input label="Confirm Password *" type="password" v-model="FormState.password.confirm"></q-input>
+          <q-input label="Confirm Password *" v-model="FormState.password.confirm"
+            :disable="!validatePassword(FormState.password.value)" type="password" :rules="[
+              (val) =>
+                (val && val === FormState.password.value) ||
+                'Must match password.',
+            ]">
+          </q-input>
         </q-card-section>
         <q-card-actions align="right">
           <q-btn flat>Cancel</q-btn>
@@ -72,11 +87,39 @@ export default {
           value: '',
           confirm: ''
         }
+      },
+      ValidPassword: {
+        length: false,
+        capital: false,
+        number: false,
+        symbol: false
       }
     }
   },
   methods: {
+    validateEmail (email) {
+      return /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email)
+    },
+    validatePassword (password) {
+      // Test length
+      this.ValidPassword.length = this.FormState.password.value.length >= 12
 
+      // Test capital
+      this.ValidPassword.capital = /^(?=.*[A-Z]).*$/.test(password)
+
+      // Test number
+      this.ValidPassword.number = /^(?=.*[0-9]).*$/.test(password)
+
+      // Test symbol
+      this.ValidPassword.symbol = /^(?=.*[!@#$%^&*-_+=]).*$/.test(password)
+
+      return (
+        this.ValidPassword.length &&
+        this.ValidPassword.capital &&
+        this.ValidPassword.number &&
+        this.ValidPassword.symbol
+      )
+    }
   },
   mounted () {
 
